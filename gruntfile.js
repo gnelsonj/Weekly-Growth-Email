10be807ca6e20955fa365fef923226ab1ca1eb73
Nelson Joyce <nellyvan@gmail.com>
2015-04-06 12:11:26 -0400

    Adding Jade stuff

---
+++ gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'sass/',
+                    cssDir: 'css/build/',
+                    outputStyle: 'compressed'
+                },
+            },
+        },
+        jade: {
+            dist: {
+                options: {
+                    pretty: true,    
+                    data: {
+                        debug: true
+                    }
+                },
+                files: {
+                    'html/weekly-digest.html': ['jade/weekly-digest.jade']
+                }
+            }
+        },
+        inlinecss: {
+            dist: {
+                options: {
+                    removeStyleTags: false,
+                    preserveMediaQueries: true
+                },
+                files: {
+                    'html/build/weekly-digest-inlined.html': 'html/weekly-digest.html'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['sass/**/*.sass', 'sass/**/*.scss'],
+                tasks: ['compass'],
+                options: {
+                    spawn: false,
+                },
+            },
+            jade: {
+                files: ['jade/**/*.jade'],
+                tasks: ['jade'],
+                options: {
+                    spawn: false,
+                },
+            },
+            inline: {
+                files: ['html/**/*.html', 'sass/**/*.sass'],
+                tasks: ['inlinecss'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-inline-css');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['compass', 'jade', 'inlinecss']);
+
+};
